65aa19b598e574665cc6d3532f91f746959c0b5c
Daniel Cespedes Zorob <daniel.cespedes.zorob@gmail.com>
2019-02-16 16:37:57 -0600

    First commits

---
+++ scrape_mission_to_mars.py
@@ -0,0 +1,27 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"} # Mac
+    # executable_path = {"executable_path": "chromedriver.exe"} # Windows
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    listings = {}
+
+    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    # listings["headline"] = soup.find("a", class_="result-title").get_text()    
+    listings["headline"] = soup.find("a", class_="content_title").get_text()
+    # listings["price"] = soup.find("span", class_="result-price").get_text()
+    # listings["hood"] = soup.find("span", class_="result-hood").get_text()
+
+    return listings
